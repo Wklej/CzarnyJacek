bdf627cc57cd9eaec6069d24d1b4df084c2629fb
Filip Wawrzyniak <kontogje@gmail.com>
2024-06-05 18:21:27 +0200

    WIP: saving results in a map, one key for one strategy

--- src/main/java/com/czarnyjacek/engine/Simulation.java
+++ src/main/java/com/czarnyjacek/engine/Simulation.java
@@ -3,10 +3,7 @@ package com.czarnyjacek.engine;
 import com.czarnyjacek.objects.Card;
 import com.czarnyjacek.objects.Result;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
@@ -18,16 +15,32 @@ public class Simulation {
                            List<Predicate<List<Card>>> splitStrategies) {
         var executorService = Executors.newFixedThreadPool(drawStrategies.size());
         List<Result> tempResults = Collections.synchronizedList(new ArrayList<>());
+        Map<Integer, List<Result>> results = Collections.synchronizedMap(new HashMap<>());
 
         IntStream.range(0, drawStrategies.size()).forEach(i ->
                 executorService.submit(() ->
                         IntStream.range(0, numberOfGames)
                                 .forEach(x -> {
                                     var game = new Game(drawStrategies.get(i), splitStrategies.get(i));
+                                    var gameResult = game.play();
                                     //TODO: send Result of simulation to kafka topic/DB
-                                     tempResults.addAll(game.play());
+                                     tempResults.addAll(gameResult);
+                                     results.computeIfAbsent(i, k -> new ArrayList<>()).addAll(gameResult);
                                 })));
 
+
         executorService.shutdown();
+        try {
+            // Wait for all tasks to finish
+            if (executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+                results.values().forEach(res -> System.out.println(
+                        res.stream()
+                                .filter(result -> result.winner() == "PLAYER")
+                                .count())
+                );
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
